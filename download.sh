e7b5acb2b0e3015b8f3e85545f7d39ec70f00596
Andy Shi <andyshi@g.harvard.edu>
2018-12-17 08:36:57 -0500

    add script to download UK Biobank summary stats

---
+++ download.sh
@@ -0,0 +1,25 @@
+# Script to download summary statistics from the Neale lab
+
+# Arm fat percentage (right), transformed
+wget https://www.dropbox.com/s/feot5hjvm18aqih/23119_irnt.gwas.imputed_v3.both_sexes.tsv.bgz?dl=0 -O 23119_irnt.gwas.imputed_v3.both_sexes.tsv.bgz
+
+# Arm fat percentage (left), transformed
+wget https://www.dropbox.com/s/6cyry111ph419lv/23123_irnt.gwas.imputed_v3.both_sexes.tsv.bgz?dl=0 -O 23123_irnt.gwas.imputed_v3.both_sexes.tsv.bgz
+
+# Trunk fat percentage, transformed
+wget https://www.dropbox.com/s/rwed4yir1o8romn/23127_irnt.gwas.imputed_v3.both_sexes.tsv.bgz?dl=0 -O 23127_irnt.gwas.imputed_v3.both_sexes.tsv.bgz
+
+# Leg fat percentage (left), transformed
+wget https://www.dropbox.com/s/x87bgk9kf6intrn/23115_irnt.gwas.imputed_v3.both_sexes.tsv.bgz?dl=0 -O 23115_irnt.gwas.imputed_v3.both_sexes.tsv.bgz
+
+# Leg fat percentage (right), transformed
+wget https://www.dropbox.com/s/jb4ak8aqhij06h5/23111_irnt.gwas.imputed_v3.both_sexes.tsv.bgz?dl=0 -O 23111_irnt.gwas.imputed_v3.both_sexes.tsv.bgz
+
+# README
+wget https://www.dropbox.com/s/wro30igqkmit5ig/README?dl=0 -O README
+
+# Table for variants
+wget https://www.dropbox.com/s/puxks683vb0omeg/variants.tsv.bgz?dl=0 -O variants.tsv.bgz
+
+# Table for phenotypes
+wget https://www.dropbox.com/s/d4mlq9ly93yhjyt/phenotypes.both_sexes.tsv.bgz -O phenotypes.both_sexes.tsv.gz
